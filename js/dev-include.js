ab3c2c2fed78492c81e49c777979c183c5f2b133
Markus Krogh <markus@casadelkrogh.dk>
2013-04-22 16:37:49 +0200

    Loading modules from json on server now, allows build automation

--- js/dev-include.js
+++ js/dev-include.js
@@ -10,8 +10,19 @@
     }
   };
 
-  load(["helpers/shims", "helpers/util", "helpers/dom"],"js/");
-  load(["models", "views", "table_sort"],"js/");
-    
+  //Load modules defined in json
+  var modulesReq = new XMLHttpRequest();
+  modulesReq.open("GET","js/modules.json", true);
+  modulesReq.responseType="text"; 
+  modulesReq.onload = function(){
+    var modules = window.JSON.parse(this.response);
 
+    load(modules["load"], modules["basePath"]);
+  }
+  modulesReq.addEventListener("error", function(){
+    //No server available
+    load(["helpers/shims", "helpers/util", "helpers/dom"],"js/");
+    load(["models", "views", "table_sort"],"js/");
+  },false);
+  modulesReq.send();
 })();
